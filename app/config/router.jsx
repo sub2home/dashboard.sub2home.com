f42abd50129ff23b15ad7482460b7f8dea59d4a3
Johannes Schickling <schickling.j@gmail.com>
2014-12-25 15:10:52 +0100

    delete routes

--- app/config/router.jsx
+++ app/config/router.jsx
@@ -1,5 +1,16 @@
-var routes = require('./routes');
+var React = require('react');
 var Router = require('react-router');
+var Route = Router.Route;
+var NotFoundRoute = Router.NotFoundRoute;
+var DefaultRoute = Router.DefaultRoute;
+var App = require('../components/App');
+var Login = require('../components/Login');
+
+var routes = (
+  <Route name="app" path="/" handler={App}>
+    <DefaultRoute handler={Login}/>
+  </Route>
+);
 
 module.exports = Router.create({
   routes: routes,
