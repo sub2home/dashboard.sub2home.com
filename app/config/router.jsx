4ee718f8a34ace218bf268d986e2008d7a180f1a
Johannes Schickling <schickling.j@gmail.com>
2015-01-01 17:27:34 +0100

    orders

--- app/config/router.jsx
+++ app/config/router.jsx
@@ -5,11 +5,11 @@ var NotFoundRoute = Router.NotFoundRoute;
 var DefaultRoute = Router.DefaultRoute;
 var App = require('../components/App');
 var Login = require('../components/Login');
-var List = require('../components/List');
+var Orders = require('../components/Orders');
 
 var routes = (
   <Route name="app" path="/" handler={App}>
-    <Route name="list" handler={List}/>
+    <Route name="orders" handler={Orders}/>
     <DefaultRoute handler={Login}/>
   </Route>
 );
