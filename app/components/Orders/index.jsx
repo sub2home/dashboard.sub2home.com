b3ba86b30473ac70ae529e312bb422b97ae17ccc
Johannes Schickling <schickling.j@gmail.com>
2015-01-01 19:39:30 +0100

    lastname

--- app/components/Orders/index.jsx
+++ app/components/Orders/index.jsx
@@ -45,7 +45,7 @@ module.exports = React.createClass({
                 <div className="orderStatus"></div>
                 <div className="orderTime">11:30</div>
                 <div className="orderDestination">
-                  <span className="orderOrderer">Schulze</span>
+                  <span className="orderOrderer">{order.addressModel.lastName}</span>
                   <span className="orderDeliveryArea">87700 Benningen</span>
                 </div>
                 <div className="orderDueTime">12:00</div>
