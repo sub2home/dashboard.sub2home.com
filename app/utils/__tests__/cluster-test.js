3b95ea79c5482dadfd9c1e29350de9bbdf34a4ef
Johannes Schickling <schickling.j@gmail.com>
2015-01-13 14:42:16 +0100

    implemented clustering

--- app/utils/__tests__/cluster-test.js
+++ app/utils/__tests__/cluster-test.js
@@ -1,6 +1,5 @@
 jest.dontMock('../cluster');
 var cluster = require('../cluster');
-var radius = 2;
 
 describe('cluster', function() {
 
@@ -13,11 +12,11 @@ describe('cluster', function() {
       center: { x: 4, y: 4 },
       points: [{ x: 4, y: 4 }]
     }];
-    expect(cluster(points, radius)).toEqual(expected);
+    expect(cluster(points, 1)).toEqual(expected);
   });
 
   it('should work for no points', function() {
-    expect(cluster([], radius)).toEqual([]);
+    expect(cluster([], 1)).toEqual([]);
   });
 
   it('should merge two points', function() {
@@ -26,7 +25,16 @@ describe('cluster', function() {
       center: { x: 2.5, y: 2.5 },
       points: points,
     }];
-    expect(cluster(points, radius)).toEqual(expected);
+    expect(cluster(points, 1)).toEqual(expected);
+  });
+
+  it('should merge three points', function() {
+    var points = [{ x: 2, y: 2 }, { x: 3, y: 3 }, { x: 4, y: 4 }];
+    var expected = [{
+      center: { x: 3, y: 3 },
+      points: points,
+    }];
+    expect(cluster(points, 1)).toEqual(expected);
   });
 
 });
--- app/utils/cluster.js
+++ app/utils/cluster.js
@@ -2,31 +2,49 @@ function intersects(p1, p2, r) {
   var dx = p1.x - p2.x;
   var dy = p1.y - p2.y;
   var r2 = r * 2;
-  var d = dx * dx + dy * dy;
-  return 0 <= d && d <= r2 * r2;
+  return dx * dx + dy * dy <= r2 * r2;
+}
+
+function mean(points) {
+  var mX = points.reduce((a, p) => a + p.x, 0);
+  var mY = points.reduce((a, p) => a + p.y, 0);
+  return {
+    x: mX / points.length,
+    y: mY / points.length,
+  };
 }
 
 module.exports = function(points, radius) {
+  if (points.length === 0) return [];
+
   var n = points.length;
   var clusterIndex = 0;
   var clusters = [];
 
+  clusters[0] = clusterIndex;
+  clusterIndex++;
+
   for (var i = 1; i < n; ++i) {
     var p1 = points[i];
-    for (var j = 0; j =< i; ++j) {
+    var intersectionFound = false;
+    for (var j = 0; j < i; ++j) {
       var p2 = points[j];
-      //if ()
+      if (intersects(p1, p2, radius)) {
+        clusters[i] = clusters[j];
+        intersectionFound = true;
+        break;
+      }
+    }
+    if (!intersectionFound) {
+      clusters[i] = clusterIndex;
+      clusterIndex++;
     }
   }
 
-  clusteredPoints = [];
-
-  for (var i = 0; i < n; ++i) {
-    clusteredPoints[i] = {
-      center: points[i],
-      points: [points[i]],
-    };
+  var clusteredPoints = [];
+  for (var i = 0; i < clusterIndex; ++i) {
+    clusteredPoints[i] = points.filter((p, j) => clusters[j] === i);
   }
 
-  return clusteredPoints;
+  return clusteredPoints.map(c => ({ center: mean(c), points: c }));
 };
